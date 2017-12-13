8f4ea551037adbaa7877e0267e4c3f2fa2d175e6
Mackay <mario.mackay@marineharvest.com>
2017-12-13 12:38:58 -0500

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,8 +77,7 @@ $(".btnadd").click(function() {
 
 	database.ref('trainSchedule/index' + indexNumber).on('child_added', function(data){
 	
-	// push data to table	
-	$("tbody").append('<tr><th scope="row">' + indexNumber + '</th><td>' + data.val().Train_Name + '</td><td>' + data.val().Destination + '</td><td>' + data.val().Frequency + '</td><td>#</td></tr>')
+	
 	console.log(data.val().Train_Time.split(':')[0]);
 		console.log(moment().format('H'));
 	
@@ -89,6 +88,77 @@ $(".btnadd").click(function() {
 	timeinMinTrain = parseInt((data.val().Train_Time.split(':')[0] * 60)) + parseInt((data.val().Train_Time.split(':')[1]));
 	console.log(timeinMin);
 	console.log(timeinMinTrain);
+		if(timeinMin >= timeinMinTrain){
+			for(var e=timeinMinTrain; e<=timeinMin; e+= data.val().Frequency){
+				console.log("E" + e + "Time: " +timeinMin);
+				if((timeinMin - e)<60){ 
+				minutesAway = (((timeinMin - e ) - data.val().Frequency)*-1);
+				}
+				if((timeinMin - e)>=60){ 
+				console.log("2nd if E" + e + "Time: " +timeinMin);
+				minutesAway = ((((timeinMin - e)% 60) - data.val().Frequency)*-1);
+				}
+			}
+				
+		}
+		
+		if(timeinMin < timeinMinTrain){
+			
+				
+				if((timeinMinTrain - timeinMin )<60){ 
+				console.log("E" + e + "Time: " +timeinMin);
+				minutesAway = (timeinMinTrain - timeinMin);
+				
+				}
+				if((timeinMinTrain - timeinMin)>=60){ 
+				console.log("2nd if E" + e + "Time: " +timeinMin);
+				var calcInt = parseInt((timeinMinTrain - timeinMin)/60);
+				var calcRemainder = ((timeinMinTrain - timeinMin) % 60);
+				console.log(calcInt);
+				console.log(calcRemainder);
+				
+				//minutesAway = (calcInt + ":" + calcRemainder);
+				minutesAway = (timeinMinTrain - timeinMin);
+				}
+			
+		}
+	// Minutes away and next arrival calculation
+			
+			//for anytime before current time
+			
+			if(timeinMin > timeinMinTrain){
+				
+				if(minutesAway < 1){
+				minutesAway = (parseInt(minutesAway) + parseInt(data.val().Frequency));
+				}
+				nextArrival= timeinMin + minutesAway;
+				console.log('nextArrival: ' + nextArrival);
+				var temp = parseInt(nextArrival / 60);
+				var temp2 = nextArrival % 60;
+				console.log ("temp" + temp);
+				console.log ("temp2" + temp2);
+				nextArrival = moment().hour(temp).minutes(temp2).format("h:mm");
+				console.log('nextArrival' + nextArrival);
+			}
+			//for future time after current time
+			
+			if(timeinMin < timeinMinTrain){
+				if(minutesAway < 1){
+				minutesAway = (parseInt(minutesAway) + parseInt(data.val().Frequency));
+				}
+				nextArrival= timeinMinTrain;
+				console.log('if 2 nextArrival: ' + nextArrival);
+				console.log();
+				var temp = parseInt(nextArrival / 60);
+				var temp2 = nextArrival % 60;
+				nextArrival = moment().hour(temp).minutes(temp2).format("h:mm");
+				console.log('nextArrival' + nextArrival);
+				
+			}
+	
+	// push data to table	
+	
+	$("tbody").append('<tr><th scope="row">' + indexNumber + '</th><td>' + data.val().Train_Name + '</td><td>' + data.val().Destination + '</td><td>' + data.val().Frequency + '</td><td>' + nextArrival + '</td><td>' + minutesAway + '</td></tr>')
 	
 	});
 	// clear input parameters
